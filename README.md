465d9628e6697a0d3aec36a0e8c7c3360ac8674e
Abdelrahman Nader <abdelrahmannohman33@gmail.com>
2026-01-21 21:11:37 +0300

    almost got recommendation algo working

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ A project to practice pandas, SQL, and git by building a movie recommendation sy
 Using MovieLens 100K dataset from GroupLens Research.
 
 ## How it works
-1. using pandas to view and clean data
+1. used pandas to view and clean data
+2. loaded cleaned and edited data to SQLite data base
+3. allowed access between code and data base with pandas through SQL query
+
+## Usability
+1. User inputs movie name
+2. algorithm finds users that have rated the movie highly (4+ stars)
+3. Then checks: What other movies did the users rate highly?
+4. Returns: Those movies as recommendations
--- scripts/Main.py
+++ scripts/Main.py
@@ -4,5 +4,29 @@ import sqlite3 as sq
 conn = sq.connect("D:/Repos/Movie_recommender/Databases/Movies.db")
 cursor = conn.cursor()
 
-statement = pd.read_sql_query(open('D:/Repos/Movie_recommender/SQL/Qeury.SQL').read(), conn)
-print(statement.to_string())
+user_input = input("What movie have you enjoyed recently: ")
+#statement = pd.read_sql_query(open('D:/Repos/Movie_recommender/SQL/Qeury.SQL').read(), conn)
+
+def find_movie_ID () -> int:
+    movie_ID_query = f"SELECT movieId FROM movies WHERE title LIKE '%{user_input}%'"
+    movie_ID = pd.read_sql(movie_ID_query, conn) #contains sequels and similar titles
+    return movie_ID.loc[0, 'movieId']  # This gets the actual integer value
+
+def find_users ()
+users_query = f"""SELECT movie-recommendations
+                FROM ratings
+                WHERE userId IN (
+                -- subquery: users who liked the input movie
+                SELECT userId 
+                FROM ratings 
+                WHERE movieId = {find_movie_ID} AND rating >= 4) AND rating >= 4"""
+    
+
+
+print(find_movie_ID())
+print(find_users())
+
+"""1. User inputs movie name
+2. algorithm finds users that have rated the movie highly (4+ stars)
+3. Then checks: What other movies did the users rate highly?
+4. Returns: Those movies as recommendations"""
